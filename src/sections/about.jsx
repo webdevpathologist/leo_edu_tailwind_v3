3e04a938fd4ce9b15a020ac2771c5b66d3776d3d
vmb-wdp <webdevpathologist@gmail.com>
2023-03-12 18:03:17 +0530

    Removed Leaflet JS&CSS, App details changed

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -4,7 +4,7 @@ import ImageSlider from '../components/imageSlider/imageSlider';
 export default function About() {
   return (
     <section id="about" className="md:px-14 md:py-10 px-4 py-6 scroll-m-24 bg-indigo-100/80 dark:bg-slate-800">
-      <div className="md:px-7 md:py-16 px-3 py-4 scroll-py-32 sm:mx-auto md:mx-7 md:my-7 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-12 bg-indigo-300/30 dark:bg-slate-700/80 hover:rounded-xl rounded-md shadow shadow-indigo-400 dark:shadow-slate-600">
+      <div className="md:px-7 md:py-16 px-3 py-4 scroll-py-32 sm:mx-auto md:mx-7 md:my-7 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-12 bg-indigo-300/30 dark:bg-slate-700/80 hover:rounded-xl rounded-md shadow shadow-indigo-400 dark:shadow-slate-600 transform transition duration-300 overflow-hidden">
           <div className="md:col-span-1">
             <AboutUs />
           </div>
